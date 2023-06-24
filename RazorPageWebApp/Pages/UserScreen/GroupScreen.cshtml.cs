51e8bf18f4413961a6b78edee94bce365b7bc223
hvpexe <phuhuynh923@gmail.com>
2023-06-24 15:05:53 +0700

    add User UI

---
+++ RazorPageWebApp/Pages/UserScreen/GroupScreen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageWebApp.Pages.UserScreen
+{
+    public class GroupScreenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RazorPageWebApp/Pages/UserScreen/HomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageWebApp.Pages.UserScreen
+{
+    public class HomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
