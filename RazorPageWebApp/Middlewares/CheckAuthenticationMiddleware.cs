813324b68123fe8fe3f1f4bd297f335d5770210a
Võ Ngọc Trúc Lam <v.trclam@gmail.com>
2023-06-25 16:28:52 +0700

    [Lam][View homepage] (#44)
    * [Lam][Edit UI, navigation path; Update login direction]
    
    * [Lam][Add check authentication middleware]

---
+++ RazorPageWebApp/Middlewares/CheckAuthenticationMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace RazorPageWebApp.Middlewares;
+
+public class CheckAuthenticationMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        var currentUser = context.Session.GetString(AppConstants.USER_NAME);
+        var path = context.Request.Path;
+        if (path.HasValue && PathCondition(path) 
+                    && !PathConditionNotInclude(path))
+        {
+            if (string.IsNullOrEmpty(currentUser))
+            {
+                context.Response.Redirect("/Auth/Login");
+                return;
+            }
+        }
+        await next(context);
+    }
+    private bool PathCondition(PathString path)
+    {
+        return (
+            path.Value.ToLower().StartsWith("/UserScreen".ToLower())
+            );
+    }
+    private bool PathConditionNotInclude(PathString path)
+    {
+        return (
+            path.Value.ToLower().StartsWith("/Index".ToLower())
+         && path.Value.ToLower().StartsWith("/Community".ToLower())
+         && path.Value.ToLower().StartsWith("/Auth".ToLower())
+            );
+    }
+}
