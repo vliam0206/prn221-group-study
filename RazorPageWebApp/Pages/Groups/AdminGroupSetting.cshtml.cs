77530c3351ec7c9a24a7acf1395f912bc7c3a5b2
viliam0206 <v.trclam@gmail.com>
2023-07-02 09:49:56 +0700

    [Lam][Add template]

---
+++ RazorPageWebApp/Pages/Groups/AdminGroupSetting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageWebApp.Pages.Groups
+{
+    public class AdminGroupSettingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
