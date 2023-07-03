af5843f6a4ab7de077889bf1544663433f85f8f6
viliam0206 <v.trclam@gmail.com>
2023-07-04 02:37:23 +0700

    [Lam][Display avatar]

--- RazorPageWebApp/AppConstants.cs
+++ RazorPageWebApp/AppConstants.cs
@@ -2,8 +2,10 @@
 
 public class AppConstants
 {
+    public static string CURRENT_USER = "CurrentUser";
     public static string USER_ID = "UserId";
     public static string USER_NAME = "UserName";
+    public static string USER_AVATAR = "UserAvatar";
     public static int GROUP_PAGE_SIZE = 8;
     public static int TOP_GROUP_NUM = 4;
 }
