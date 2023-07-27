14af9376680fe1a36a015e702253b90f0b97a43a
Bình Nguyen Thanh <binhnguyenthanh19242yahoo@gmail.com>
2023-07-27 17:30:17 +0700

    Like 80%

--- RazorPageWebApp/wwwroot/js/post.js
+++ RazorPageWebApp/wwwroot/js/post.js
@@ -1,11 +1,14 @@
 
 function toggleLike(entity) {
-    var postId = $(entity).data('postId');
+    var postId = $(entity).data('postid');
     var url = new URL(location);
     url.searchParams.append('handler', "Like");
     $.ajax({
         url: url,
         method: 'post',
+        data: {
+            postId
+        },
         success: function (res) {
             console.log(res);
         },
